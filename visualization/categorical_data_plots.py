2f854a6cf280d8b5e1030c0cdbd90252feea3466
michal <m.kozel92@gmail.com>
2019-05-24 20:24:44 +0800

    Added initial version of data dashboard
    This includes scatter plot for categorical data and histogram of target distribution

---
+++ visualization/categorical_data_plots.py
@@ -0,0 +1,53 @@
+import plotly.graph_objs as go
+import numpy as np
+
+from visualization.colors import BLUE_SCHEME
+
+
+def get_categorical_data_scatter(data: np.array, labels: np.array, name: str, highlight: str = None,
+                                 color_scheme: list = BLUE_SCHEME) -> dict:
+    np.unique(labels)
+    traces = []
+
+    for i, label in enumerate(np.unique(labels)):
+        mask = labels == label
+        label_data = data[mask]
+
+        if label == highlight:
+            opacity = 1
+            size = 12
+        else:
+            opacity = 0.8
+            size = 10
+
+        trace = go.Scatter(
+            x=label_data[:, 0],
+            y=label_data[:, 1],
+            name='%d' % label,
+            mode='markers',
+            marker=dict(
+                size=size,
+                color=color_scheme[i],
+                opacity=opacity,
+                line=dict(
+                    width=0,
+                )
+            )
+        )
+        traces.append(trace)
+
+    layout = dict(title=name,
+                  yaxis=dict(zeroline=False),
+                  xaxis=dict(zeroline=False)
+                  )
+    return {'layout': layout, 'data': traces}
+
+
+def get_target_histogram(targets: np.array, name: str, color_scheme: list = BLUE_SCHEME) -> dict:
+    traces = [go.Histogram(
+        x=targets,
+        marker=dict(color=color_scheme[1])
+    )]
+    layout = dict(title=name)
+    return {'layout': layout, 'data': traces}
+
---
+++ visualization/visualization_app.py
@@ -0,0 +1,49 @@
+import numpy as np
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+from data.data_generation import get_categorical_gaussian_data
+from visualization.categorical_data_plots import get_categorical_data_scatter, get_target_histogram
+
+app = dash.Dash()
+
+centers = np.array([[1, 1], [5, 5], [10, 10], [20, 20], [5,6]])
+sigmas = np.array([1, 1, 1, 1, 5])
+counts = np.array([10, 20, 30, 50, 10])
+
+data, labels = get_categorical_gaussian_data(centers=centers, sigmas=sigmas, counts=counts)
+
+fig_0 = get_categorical_data_scatter(data=data, labels=labels, name='sample data')
+fig_1 = get_target_histogram(targets=labels, name='targets histogram')
+
+app.layout = html.Div(
+    [html.Div([html.H1("Data dashboard")], style={'textAlign': "center", 'padding': 10}),
+     html.Div([html.Div([dcc.Graph(id="scatter-graph",
+                                   figure=fig_0)],
+                        className="six columns",
+                        style={'width': '49%', 'display': 'inline-block'}
+                        ),
+               html.Div([dcc.Graph(id="hist-graph",
+                                   figure=fig_1,
+                                   clear_on_unhover=True, )],
+                        className="six columns",
+                        style={'width': '49%', 'display': 'inline-block'}),
+               ]),
+     ],
+    className="container")
+
+
+@app.callback(
+    dash.dependencies.Output("scatter-graph", "figure"),
+    [dash.dependencies.Input("hist-graph", "hoverData")])
+def update_scatter(hoverData):
+    if hoverData is not None:
+        label = hoverData["points"][0]['x']
+        return get_categorical_data_scatter(data=data, labels=labels, name='sample data scatter', highlight=label)
+    else:
+        return get_categorical_data_scatter(data=data, labels=labels, name='sample data scatter')
+
+
+if __name__ == '__main__':
+    app.run_server()
